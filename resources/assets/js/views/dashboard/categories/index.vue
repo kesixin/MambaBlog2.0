f4fdd15358364598b92eac308fe0c4a6762f9c88
kesixin <462369233@qq.com>
2018-09-25 18:38:31 +0800

    create categories

--- resources/assets/js/views/dashboard/categories/index.vue
+++ resources/assets/js/views/dashboard/categories/index.vue
@@ -1,3 +1,23 @@
 <template>
-    <div>asdasd</div>
+    <div>
+        <Row>
+            <Col sapn="24">
+                <Card>
+                    <p slot="title" style="height:100%;text-align: right">
+                        <router-link to="/categories/add">
+                            <Button type="primary" icon="android-add-circle">添加分类</Button>
+                        </router-link>
+                    </p>
+                    <Table  :loading="loading" :data="tableData" :columns="tableColumns" stripe>
+                        <div style="margin: 10px;overflow: hidden">
+                            <div style="float:right">
+
+                                2
+                            </div>
+                        </div>
+                    </Table>
+                </Card>
+            </Col>
+        </Row>
+    </div>
 </template>
